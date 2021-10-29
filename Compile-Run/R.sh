f830a6a61205b926872cf240462f04ba1fb0a6fe
jason-fxz <jasonfanxz@gmail.com>
2021-10-29 18:34:51 +0800

    upd Compile-Run/R.sh

--- Compile-Run/R.sh
+++ Compile-Run/R.sh
@@ -1,21 +1,28 @@
-#!/usr/bin/sh
+#!/usr/bin/bash
+echo () {
+    builtin echo -e $* >&2
+}
+run_code() {
+    echo "================= begin ================="
+    time -p "./$1"
+}
 flags='-Wall -Wextra -fsanitize=undefined -g3'
 name="$1"
 echo "\033[1;32mINFO:\033[0m WorkDir: \033[35m$(pwd)\033[0m"
 if [ $# -eq 0 ]; then
     echo "\033[1;31mERR:\033[0m Please Enter File!"
 elif [ ! -e "$name.cpp" ]; then
-    echo "\033[1;31mERR:\033[0m No that File \033[35m$name.cpp\033[0m"
+    echo "\033[1;31mERR:\033[0m No that File \033[35m'$name.cpp'\033[0m"
 else
     if [ ! -e ".$name.bak" ] || [ -n "$(diff -q ".$name.bak" "$name.cpp")" ]; then
-        echo "\033[1;32mINFO:\033[0m Compile & Run \033[35m$name.cpp\033[0m"
+        echo "\033[1;32mINFO:\033[0m Compile & Run \033[35m'$name.cpp'\033[0m"
         g++ $flags "$name.cpp" -o "$name.run"
         if [ $? -eq 0 ]; then 
             cp "$name.cpp" ".$name.bak"
-            echo "================= begin =================" && time -p ./"$name.run"
+            run_code "$name.run"
         fi
     else
-        echo "\033[1;32mINFO: \033[0;35m$name.cpp\033[0m is up to date"
-        echo "================= begin =================" && time -p ./"$name.run"
+        echo "\033[1;32mINFO: \033[0;35m'$name.cpp'\033[0m is up to date"
+        run_code "$name.run"
     fi    
 fi
